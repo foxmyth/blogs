d1927ebf3fa8c6ec25afde8b98bb390f8f3f8103
michelle/LEE,Hyunjung <hjlee@crscube.co.kr>
2016-09-19 18:26:14 +0900

    메일 송신 작업중

--- app/Services/AuthService.php
+++ app/Services/AuthService.php
@@ -3,14 +3,32 @@
 namespace App\Services;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 /**
-* 
+*
+*
+* https://tuts.codingo.me/laravel-social-and-email-authentication
+* https://github.com/codingo-me/laravel-social-email-authentication
 */
 class AuthService 
 {
-	
-	public function sendResetMail(Request $request) 
+	public function __construct(Mail $mail)
 	{
+		$this->mail = $mail;
+	}
+	
+	public function sendResetMail(Request $request, $subject)
+	{
+		//TODO: reset mail의 view 작업하기
+		
+		$data = $request->only('name', 'email');
+
+		$this->mail->send('email.reset', 
+		                  [''],
+		                  function($message) use ($data) {
+		                  	$message->subject($subject)
+		                  		 	->to($data->email);		                  		 	
+		                  }
 	}
 }
